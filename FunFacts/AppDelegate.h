4d6011e69bd66ff8f9f740f7a3cf8067afafe5e0
John Hirakawa <johnhirakawa@Johns-MacBook-Pro.local>
2015-06-05 16:55:22 -0400

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by John Hirakawa on 6/5/15.
+//  Copyright (c) 2015 Tree House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by John Hirakawa on 6/5/15.
+//  Copyright (c) 2015 Tree House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
